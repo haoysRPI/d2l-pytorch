cf6d71c6653ca029737f94c667a7d28e77428e3b
haoysRPI <erichao1988@gmail.com>
2020-05-16 18:22:08 -0700

    update the chapter names

--- d2l/data/fashion_mnist.py
+++ d2l/data/fashion_mnist.py
@@ -1,28 +1,44 @@
 import os
 import sys
+import torch
 import torchvision
 from torchvision import transforms
-from torch.utils.data import DataLoader
-from ..figure import plt, use_svg_display
+from torch.utils.data import DataLoader, TensorDataset
+from matplotlib import pyplot as plt
 
 
 def load_data_fashion_mnist(batch_size, resize=None, root=os.path.join(
         '~', '.pytorch', 'datasets', 'fashion-mnist')):
     """Download the Fashion-MNIST dataset and then load into memory."""
-    root = os.path.expanduser(root)
-    transformer = []
-    if resize:
-        transformer += [transforms.Resize(resize)]
-    transformer += [transforms.ToTensor()]
-    transformer = transforms.Compose(transformer)
 
-    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, transform=transformer, download=True)
-    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, transform=transformer, download=True)
+    train_data_path = '/Users/yingshuaihao/PycharmProjects/Dataset/FashionMNIST/processed/training.pt'
+    test_data_path = '/Users/yingshuaihao/PycharmProjects/Dataset/FashionMNIST/processed/test.pt'
+
+    if os.path.exists(train_data_path) and os.path.exists(test_data_path):
+        X, y = torch.load(train_data_path)
+        X = X.float()
+        mnist_train = TensorDataset(X, y)
+
+        X, y = torch.load(test_data_path)
+        X = X.float()
+        mnist_test = TensorDataset(X, y) 
+    else:
+        root = os.path.expanduser(root)
+        transformer = []
+        if resize:
+            transformer += [transforms.Resize(resize)]
+        transformer += [transforms.ToTensor()]
+        transformer = transforms.Compose(transformer)
+
+        mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, transform=transformer, download=True)
+        mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, transform=transformer, download=True)
+
     num_workers = 0 if sys.platform.startswith('win32') else 4
 
     train_iter = DataLoader(mnist_train, batch_size, shuffle=True, num_workers=num_workers)
     test_iter = DataLoader(mnist_test, batch_size, shuffle=False, num_workers=num_workers)
     return train_iter, test_iter
+
 
 def get_fashion_mnist_labels(labels):
     """Get text labels for Fashion-MNIST."""
@@ -33,7 +49,10 @@ def get_fashion_mnist_labels(labels):
 
 def show_fashion_mnist(images, labels):
     """Plot Fashion-MNIST images with labels."""
-    use_svg_display()
+    from IPython import display
+    display.set_matplotlib_formats('svg')
+
+    # use_svg_display()
     _, figs = plt.subplots(1, len(images), figsize=(12, 12))
     for f, img, lbl in zip(figs, images, labels):
         f.imshow(img.reshape((28, 28)).numpy())
